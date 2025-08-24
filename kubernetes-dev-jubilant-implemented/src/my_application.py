54db170d1da7ebcec3add0731a4739c5e880e64d
Dave Wilding <tech@dpw.me>
2025-08-24 18:10:22 +0800

    add implemented kubernetes charm

---
+++ kubernetes-dev-jubilant-implemented/src/my_application.py
@@ -0,0 +1,23 @@
+# Copyright 2025 david.wilding@canonical.com
+# See LICENSE file for licensing details.
+
+"""Functions for interacting with the workload.
+
+The intention is that this module could be used outside the context of a charm.
+"""
+
+import logging
+
+import requests
+
+logger = logging.getLogger(__name__)
+
+
+# Functions for interacting with the workload, for example over HTTP:
+
+
+def get_version() -> str | None:
+    """Get the running version of the workload."""
+    response = requests.get("http://localhost:8000/version")
+    resonse_data = response.json()
+    return resonse_data["version"]
