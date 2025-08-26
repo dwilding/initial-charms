2895ae0a13cfd0a1b4bc902a25ca21cd4033850f
Dave Wilding <tech@dpw.me>
2025-08-26 11:40:36 +0800

    add docstrings to file rewriter

--- implemented/rewriter.py
+++ implemented/rewriter.py
@@ -12,6 +12,7 @@ class Rewriter:
     def next_by_prefix(
         self, prefix: str, *, change: str | None = None, remove_line: bool = False
     ):
+        """Set the current location to the next line that matches a prefix."""
         for line_index in range(self._first_line_index, len(self._lines)):
             line = self._lines[line_index]
             if line.startswith(prefix):
@@ -26,6 +27,7 @@ class Rewriter:
         raise ValueError('no matching line')
 
     def insert(self, new: str):
+        """Insert a line at the current location."""
         self._lines.insert(self._first_line_index, new)
         self._first_line_index += 1
 
