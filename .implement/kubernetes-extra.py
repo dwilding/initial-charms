c34dad9bd69914a47668d7d2939b11b8e3d34f42
Dave Wilding <tech@dpw.me>
2025-09-05 09:01:52 +0800

    adjust implementation code

--- .implement/kubernetes-extra.py
+++ .implement/kubernetes-extra.py
@@ -7,7 +7,7 @@ def main():
     # Change the OCI image to the demo server from the K8s charm tutorial:
     # https://documentation.ubuntu.com/ops/latest/tutorial/from-zero-to-hero-write-your-first-kubernetes-charm/study-your-application/
     r = rewriter.Rewriter('charmcraft.yaml')
-    r.next_by_prefix(
+    r.next_p(
         prefix='    upstream-source: some-repo/some-image:some-tag',
         change='    upstream-source: ghcr.io/canonical/api_demo_server:1.0.1',
     )
@@ -16,14 +16,14 @@ def main():
     # Change the Pebble layer so that Pebble starts the server.
     r = rewriter.Rewriter('src/charm.py')
     r.set_indent(4)
-    r.next_by_prefix('def _on_pebble_ready')
-    r.next_by_prefix('    layer')
+    r.next_p('def _on_pebble_ready')
+    r.next_p('    layer')
     r.insert(
         '    command = "uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000"',
         offset=-1,
     )
     r.set_indent(5 * 4)
-    r.next_by_prefix(
+    r.next_p(
         prefix='"command": "/bin/foo"',
         change='"command": command',
     )
@@ -32,11 +32,11 @@ def main():
     # Implement get_version() in src/my_application.py, by requesting the version over HTTP.
     subprocess.check_call(['uv', 'add', '--quiet', 'requests'])  # Add package to charm venv.
     r = rewriter.Rewriter('src/my_application.py')
-    r.next_by_prefix('import logging')
+    r.next_p('import logging')
     r.insert('')
     r.insert('import requests')
-    r.next_by_prefix('def get_version()')
-    r.next_by_prefix('    return', remove_line=True)
+    r.next_p('def get_version()')
+    r.next_p('    return', remove_line=True)
     r.insert("""\
     response = requests.get("http://localhost:8000/version")
     resonse_data = response.json()
--- .implement/rewriter.py
+++ .implement/rewriter.py
@@ -14,8 +14,11 @@ class Rewriter:
         """Set the number of spaces for indentation."""
         self._indent = ' ' * length
 
-    def next_by_prefix(self, prefix: str, *, change: str | None = None, remove_line: bool = False):
+    def next_p(self, prefix: str, *, change: str | None = None, remove_line: bool = False):
         """Set the current location to the next line that matches a prefix."""
+        # To help make calling code more readable, it's important that the name of this method has
+        # six characters, to align with insert(). It's also important that 'prefix' and 'change'
+        # have the same number of characters.
         prefix_with_indent = self._indent + prefix
         for line_index in range(self._first_line_index, len(self._lines)):
             line = self._lines[line_index]
